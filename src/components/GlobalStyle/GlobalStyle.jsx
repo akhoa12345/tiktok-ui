1df27bd4abd0d6c5d5a8234264c58d5167131d6a
Anh Khoa <phunganhkhoa123@gmail.com>
2023-11-14 19:32:31 +0700

    Tạo GlobalStyles component, cài styled-component, Reset CSS, Default CSS

---
+++ src/components/GlobalStyle/GlobalStyle.jsx
@@ -0,0 +1,23 @@
+import { createGlobalStyle } from 'styled-components';
+import { normalize } from 'styled-normalize';
+
+const GlobalStyle = createGlobalStyle`
+  ${normalize}
+
+  :root {
+    --primary: #fe2c55
+  }
+
+  html {
+    font-size: 65.5%
+  }
+
+  body {
+    font-family: 'IBM Plex Sans', sans-serif;
+    font-size: 1.6rem;
+    line-height: 1.5;
+    text-rendering: optimizeSpeed;
+  }
+`;
+
+export default GlobalStyle;
